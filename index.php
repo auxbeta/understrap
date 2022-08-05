a67a0c797e4d7ae1aa319eaf2f9e89ea63f41941
pat auxbeta <pat@auxbeta.com>
2022-08-05 10:03:22 -0500

    Added Aux Beta SCSS to sass>theme with scss file in sass>theme>theme

--- index.php
+++ index.php
@@ -29,10 +29,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<div class="row">
 
-			<!-- Do the left sidebar check and opens the primary div -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
 			<main class="site-main" id="main">
+				<div class="col-6">
 				<?php
 				if ( have_posts() ) {
 					// Start the Loop.
@@ -50,14 +49,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 					get_template_part( 'loop-templates/content', 'none' );
 				}
 				?>
-
+				</div>
 			</main><!-- #main -->
 
 			<!-- The pagination component -->
 			<?php understrap_pagination(); ?>
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
 		</div><!-- .row -->
 
