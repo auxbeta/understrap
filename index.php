67cdfb1cb1ae3097f415aed7f07b8621694d7d62
pat auxbeta <pat@auxbeta.com>
2022-08-05 12:44:10 -0500

    Blog Page Layout

--- index.php
+++ index.php
@@ -27,36 +27,29 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-		<div class="row">
-
-
 			<main class="site-main" id="main">
-				<div class="col-6">
-				<?php
-				if ( have_posts() ) {
-					// Start the Loop.
-					while ( have_posts() ) {
-						the_post();
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-					}
-				} else {
-					get_template_part( 'loop-templates/content', 'none' );
-				}
-				?>
-				</div>
+				<div class="row">
+						<?php
+						if ( have_posts() ) {
+							// Start the Loop.
+							while ( have_posts() ) {
+								the_post();
+		
+								/*
+								 * Include the Post-Format-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								get_template_part( 'loop-templates/content', get_post_format() );
+							}
+						} else {
+							get_template_part( 'loop-templates/content', 'none' );
+						}
+						?>
+				</div><!-- .row -->
 			</main><!-- #main -->
-
 			<!-- The pagination component -->
 			<?php understrap_pagination(); ?>
-
-
-		</div><!-- .row -->
 
 	</div><!-- #content -->
 
